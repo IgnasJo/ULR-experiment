d76ad8e55860007246fa5a7ac30159bc0e47de4c
IgnasJ <jogminasignas@gmail.com>
2026-01-08 23:37:44 +0200

    Refactored-expanded config

--- config.py
+++ config.py
@@ -1,17 +1,52 @@
-import os
-import torch
+"""Global configuration values, various training, evaluation parameters, magic values. Can override with local_config.py"""
 
-LOAD_MODEL = False
-SAVE_MODEL = True
-LEARNING_RATE = 1e-4
-NUM_EPOCHS = 100
-BATCH_SIZE = 16
+import torch
+from types import SimpleNamespace
+
+
+evaluation_config = SimpleNamespace(
+  test_dir = r'custom\customULR\custom_rgb',
+  test_dir_gt = r'custom\customULR\custom_label',
+  checkpoint_path = r'checkpoints\best\joint_checkpoint_best.pth',
+  evaluation_dir = 'evaluation_output',
+  evaluation_checkpoint_path = r'checkpoints\evaluation_checkpoint'
+)
+
+format_config = SimpleNamespace(
+  ultra_low_resolution = 16,
+  low_resolution = 96,
+  img_channels = 3
+)
+format_config.high_resolution = format_config.low_resolution * 4
+
+
+
+# -------- Training --------
+training_config = SimpleNamespace(
+num_classes = 14, # Count with default "background" class
+num_epochs = 100,
+batch_size = 1,
+generator_lr = 1e-4,
+discriminator_lr = 1e-4,
+segmentor_lr = 1e-2,
+lr_scheduler = 'poly',
+LEARNING_RATE = 1e-4,
+# Loss Weights
+alpha = 0.3,     # Balancing parameter between Generative and Segmentation loss
+lambda_1 = 0.5,  # Weight for L2 Pixel Loss
+lambda_2 = 0.01, # Weight for Feature Loss
+lambda_3 = 0.01, # Weight for Adversarial Loss
+image_dir = r'datasets\custom_ULR\custom_rgb',
+mask_dir = r'datasets\custom_ULR\custom_label'
+)
+
 LAMBDA_GP = 10
-LOW_RES = 96
-HIGH_RES = LOW_RES * 4
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 INTERMEDIA_SIZE = 16
 
-IMG_CHANNELS = 3
-
+# Try to override config with local_config.py
+try:
+    from local_config import *
+except ImportError:
+    pass
