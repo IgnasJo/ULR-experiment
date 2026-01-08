0d55d77f7446ee4ea1858c4a5863fb2074842f9a
IgnasJ <jogminasignas@gmail.com>
2026-01-08 23:36:57 +0200

    Patched metrics

--- utils2/metrics.py
+++ utils2/metrics.py
@@ -11,7 +11,9 @@ class Evaluator(object):
         return Acc
 
     def Pixel_Accuracy_Class(self):
-        Acc = np.diag(self.confusion_matrix) / self.confusion_matrix.sum(axis=1)
+        # Ignore classes that were not yet seen in output or ground truth labels
+        with np.errstate(divide='ignore', invalid='ignore'):
+            Acc = np.diag(self.confusion_matrix) / self.confusion_matrix.sum(axis=1)
         Acc = np.nanmean(Acc)
         return Acc
 
@@ -32,7 +34,10 @@ class Evaluator(object):
         return FWIoU
 
     def _generate_matrix(self, gt_image, pre_image):
-        mask = (gt_image >= 0) & (gt_image < self.num_class)
+        mask = (
+        (gt_image >= 0) & (gt_image < self.num_class) &
+        (pre_image >= 0) & (pre_image < self.num_class)
+        )
         label = self.num_class * gt_image[mask].astype('int') + pre_image[mask]
         count = np.bincount(label, minlength=self.num_class**2)
         confusion_matrix = count.reshape(self.num_class, self.num_class)
