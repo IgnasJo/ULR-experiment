18570f076186fff0f4fed335406139900aa50f77
IgnasJ <jogminasignas@gmail.com>
2026-01-11 16:19:43 +0200

    Add pretraining config

--- config.py
+++ config.py
@@ -20,30 +20,35 @@ format_config = SimpleNamespace(
 format_config.high_resolution = format_config.low_resolution * 4
 
 
-
-# -------- Training --------
 training_config = SimpleNamespace(
-num_classes = 14, # Count with default "background" class
-num_epochs = 100,
-batch_size = 1,
-generator_lr = 1e-4,
-discriminator_lr = 1e-4,
-segmentor_lr = 1e-2,
-lr_scheduler = 'poly',
-LEARNING_RATE = 1e-4,
-# Loss Weights
-alpha = 0.3,     # Balancing parameter between Generative and Segmentation loss
-lambda_1 = 0.5,  # Weight for L2 Pixel Loss
-lambda_2 = 0.01, # Weight for Feature Loss
-lambda_3 = 0.01, # Weight for Adversarial Loss
-image_dir = r'datasets\custom_ULR\custom_rgb',
-mask_dir = r'datasets\custom_ULR\custom_label'
+  num_classes = 14, # Count with default "background" class
+  num_epochs = 100,
+  batch_size = 1,
+  generator_lr = 1e-4,
+  discriminator_lr = 1e-4,
+  segmentor_lr = 1e-2,
+  lr_scheduler = 'poly',
+  LEARNING_RATE = 1e-4,
+  # Loss Weights
+  alpha = 0.3,     # Balancing parameter between Generative and Segmentation loss
+  lambda_1 = 0.5,  # Weight for L2 Pixel Loss
+  lambda_2 = 0.01, # Weight for Feature Loss
+  lambda_3 = 0.01, # Weight for Adversarial Loss
+  image_dir = r'datasets\custom_demo\custom_rgb',
+  mask_dir = r'datasets\custom_demo\custom_label'
 )
 
-LAMBDA_GP = 10
+pretraining_config = SimpleNamespace(
+  num_epochs = 50,
+  vgg_weight = 5e-3,
+  gan_weight = 1e-2,
+  batch_size = 16,
+  hr_image_dir=r'datasets\custom_demo\custom_rgb',
+  generator_lr = training_config.generator_lr,
+  discriminator_lr = training_config.discriminator_lr,
+)
 
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-INTERMEDIA_SIZE = 16
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Try to override config with local_config.py
 try:
